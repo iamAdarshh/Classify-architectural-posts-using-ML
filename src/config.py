38b5d52a8184a6061ade429755cc99606aa6c534
Adarsh Choudhary <44595895+iamAdarshh@users.noreply.github.com>
2024-06-15 11:19:55 +0200

    build: configure pylint (#1)
    * Create pylint.yml
    
    * fix: lint issues

--- src/config.py
+++ src/config.py
@@ -1,3 +1,13 @@
+"""
+This module provides a constant for the default data folder path.
+
+The `DEFAULT_DATA_FOLDER` is set to a path that points to a 'data' directory
+located in the parent directory of the current file.
+
+Attributes:
+    DEFAULT_DATA_FOLDER (str): The default path to the data folder.
+"""
+
 import os
 
 DEFAULT_DATA_FOLDER = os.path.join(os.path.dirname(__file__), '..', 'data')
