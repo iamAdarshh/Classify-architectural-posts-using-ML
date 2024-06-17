6c6d22413a2b1528be5681420c94cccc31d8a016
Adarsh Choudhary <adarshchoudhary087@gmail.com>
2024-06-17 16:24:59 +0200

    fix: apikey should not be exposed

--- src/utils/posts_helpers.py
+++ src/utils/posts_helpers.py
@@ -10,16 +10,28 @@ The main steps include:
 2. Fetching the posts for each batch.
 3. Returning the combined list of posts.
 """
-
+import os.path
 from typing import List, Dict, Tuple, Optional
 import time
 import requests
 
+from src.config import API_KEY_PATH
+
 POST_BASE_URL = "https://api.stackexchange.com/2.3/questions/{}"
 ANSWER_BASE_URL = "https://api.stackexchange.com/2.2/questions/{}/answers"
+
+
+def read_api_key(file_path: str) -> str:
+    """Read the API key from a text file."""
+    with open(file_path, 'r') as file:
+        return file.read().strip()
+
+
+API_KEY = read_api_key(API_KEY_PATH)
+
 PARAMS = {
     "site": "stackoverflow",
-    "key": "rl_cMc4Gvbc6zeCSRtpyw18cVHQK",
+    "key": API_KEY,
     "filter": "withbody"  # Include the post body in the response
 }
 
